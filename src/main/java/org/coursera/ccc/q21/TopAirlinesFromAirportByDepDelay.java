1c2d1b6a87aca9a51b14c3ac3e6ed2445a6dd9b0
developer bach <developer.bach@google.com>
2016-02-20 23:32:18 +0100

    bugfix test

--- src/main/java/org/coursera/ccc/q21/TopAirlinesFromAirportByDepDelay.java
+++ src/main/java/org/coursera/ccc/q21/TopAirlinesFromAirportByDepDelay.java
@@ -120,7 +120,7 @@ public final class TopAirlinesFromAirportByDepDelay
 
 		try (JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(10))) {
 
-			prepareCassandraKeyspace(jssc);
+	//		prepareCassandraKeyspace(jssc);
 
 			// must set for statefull operations
 			jssc.checkpoint(".");
@@ -153,8 +153,8 @@ public final class TopAirlinesFromAirportByDepDelay
 			};
 			JavaDStream<CarrierDelayEntity> carrierDelays = performance.transform(transformFunc);
 
-			CassandraStreamingJavaUtil.javaFunctions(carrierDelays)
-					.writerBuilder(CASSANDRA_KEYSPACE, CASSANDRA_TABLE, CassandraJavaUtil.mapToRow(CarrierDelayEntity.class)).saveToCassandra();
+//			CassandraStreamingJavaUtil.javaFunctions(carrierDelays)
+//					.writerBuilder(CASSANDRA_KEYSPACE, CASSANDRA_TABLE, CassandraJavaUtil.mapToRow(CarrierDelayEntity.class)).saveToCassandra();
 
 			performance.foreachRDD(rdd -> {
 				List<Tuple2<String, Set<CarrierDelay>>> topCarriersByDelay = rdd.take(10);
