88e192ff3f342e517f9415521accc3f516ead5d5
developer bach <developer.bach@google.com>
2016-02-21 10:07:28 +0100

    bugfix

--- src/main/java/org/coursera/ccc/q21/TopAirlinesFromAirportByDepDelay.java
+++ src/main/java/org/coursera/ccc/q21/TopAirlinesFromAirportByDepDelay.java
@@ -83,7 +83,7 @@ public final class TopAirlinesFromAirportByDepDelay
 	private static Function2<List<CarrierDelay>, Optional<Set<CarrierDelay>>, Optional<Set<CarrierDelay>>> mergeOrigins = (newRecords, currentRecords) -> {
 		Set<CarrierDelay> agg = currentRecords.or(new TreeSet<>());
 		agg.addAll(newRecords);
-		return Optional.of(new TreeSet<>(agg.stream().limit(10).collect(Collectors.toSet())));
+		return Optional.of(new TreeSet<>(agg.stream().limit(100).collect(Collectors.toSet())));
 	};
 
 	private static Function<Tuple2<String, String>, String> mapLines = x -> x._2();
